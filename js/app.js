2c60c2977a9ded60167387f46a9fb4aab1f0e058
ma7aziz <45315298+ma7aziz@users.noreply.github.com>
2021-08-19 18:07:27 +0200

    built the nav

--- js/app.js
+++ js/app.js
@@ -18,22 +18,12 @@
  *
  */
 
-// SECTIONS
-// let section1 = document.getElementById("section1");
-// let section2 = document.getElementById("section2");
-// let section3 = document.getElementById("section3");
-// let section4 = document.getElementById("section4");
-
-// // ALL SECTIONS
-// let sections = document.querySelectorAll("section");
-// // console.log(sections);
-// for (let section of sections) {
-//   console.log(section.querySelector("h2").innerText);
-// }
-// document.querySelector("#section1 > div > h2");
-// // NAVBAR
-// let navList = document.getElementById("navbar__list");
-
+let section1 = document.getElementById("section1");
+let section2 = document.getElementById("section2");
+let section3 = document.getElementById("section3");
+let section4 = document.getElementById("section4");
+let sections = document.querySelectorAll("section");
+let navList = document.getElementById("navbar__list");
 /**
  * End Global Variables
  * Start Helper Functions
@@ -47,7 +37,19 @@
  */
 
 // build the nav
-
+for (let section of sections) {
+  // create new list item //
+  let newLi = document.createElement("li");
+  let linkText = document.createTextNode(section.querySelector("h2").innerText);
+  navList.appendChild(newLi);
+  // create new nection link and append it to list item
+  let link = document.createElement("a");
+  newLi.appendChild(link);
+  link.appendChild(linkText);
+  // set link href  == section id
+  link.setAttribute("href", `#${section.getAttribute("id")}`);
+  newLi.style.cssText = "color : red ; padding : 10px ";
+}
 // Add class 'active' to section when near top of viewport
 
 // Scroll to anchor ID using scrollTO event
