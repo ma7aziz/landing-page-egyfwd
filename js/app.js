3f55e9186dad7e9946688dac7d7911eec3fb34da
ma7aziz <45315298+ma7aziz@users.noreply.github.com>
2021-08-22 18:16:27 +0200

    scroll into section // add active class to section

--- js/app.js
+++ js/app.js
@@ -30,6 +30,17 @@ let navList = document.getElementById("navbar__list");
  *
  */
 
+let deleteActiveClasses = () => {
+  let li = document.querySelectorAll("nav li ");
+  li.forEach((l) => {
+    l.classList.remove("active");
+  });
+};
+
+let addActiveClass = function (section) {
+  let selector = `nav li a[href="#${section}"]`;
+  document.querySelector(selector).parentElement.classList.add("active");
+};
 /**
  * End Helper Functions
  * Begin Main Functions
@@ -37,21 +48,37 @@ let navList = document.getElementById("navbar__list");
  */
 
 // build the nav
-for (let section of sections) {
-  // create new list item //
-  let newLi = document.createElement("li");
-  let linkText = document.createTextNode(section.querySelector("h2").innerText);
-  navList.appendChild(newLi);
-  // create new nection link and append it to list item
-  let link = document.createElement("a");
-  newLi.appendChild(link);
-  link.appendChild(linkText);
-  // set link href  == section id
-  link.setAttribute("href", `#${section.getAttribute("id")}`);
-  newLi.style.cssText = "color : red ; padding : 10px ";
-}
+let buildNav = function () {
+  for (let section of sections) {
+    // create new list item //
+    let newLi = document.createElement("li");
+    let linkText = document.createTextNode(
+      section.querySelector("h2").innerText
+    );
+    navList.appendChild(newLi);
+    // create new nection link and append it to list item
+    let link = document.createElement("a");
+    newLi.appendChild(link);
+    link.appendChild(linkText);
+    // set link href  == section id
+    link.setAttribute("href", `#${section.getAttribute("id")}`);
+    newLi.style.cssText = "color : red ; padding : 10px ";
+  }
+};
 // Add class 'active' to section when near top of viewport
-
+onscroll = function () {
+  let scrollPosition = document.documentElement.scrollTop || window.pageYOffset;
+  sections.forEach((section) => {
+    if (
+      scrollPosition >= section.offsetTop - section.offsetHeight * 0.3 &&
+      scrollPosition <
+        section.offsetTop + section.offsetHeight - section.offsetHeight * 0.3
+    ) {
+      deleteActiveClasses();
+      addActiveClass(section.attributes.id.value);
+    }
+  });
+};
 // Scroll to anchor ID using scrollTO event
 
 /**
@@ -61,7 +88,29 @@ for (let section of sections) {
  */
 
 // Build menu
-
+document.addEventListener("load", buildNav());
 // Scroll to section on link click
 
 // Set sections as active
+
+/// SCROLL TO TOP OR BOTTOM
+let scrolBtn = document.getElementById("scroll");
+onload = function () {
+  scrolBtn.innerText = "scroll to buttom";
+};
+
+navList.addEventListener("click", (e) => {
+  e.preventDefault();
+  let selector = e.target.attributes.href.value;
+  if (selector) {
+    let section = document.querySelector(selector);
+    section.scrollIntoView({
+      behavior: "smooth",
+      block: "center",
+    });
+    sections.forEach((section) =>
+      section.classList.remove("your-active-class")
+    );
+    section.classList.add("your-active-class");
+  }
+});
